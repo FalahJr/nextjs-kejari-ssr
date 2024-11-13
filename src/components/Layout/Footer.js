54bb32bdd8f2aaaed35280a9712f1adace7aa61e
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-11-14 03:31:39 +0700

    fix (ui) : update footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -7,35 +7,88 @@ const Footer = () => {
   return (
     <footer className="bg-[#17438E]">
       <div className="container mx-auto py-6 lg:pt-16 lg:pb-8 text-white">
-        <div className="mt-6 grid grid-cols-1 gap-10 lg:gap-0 lg:grid-cols-3 w-full px-6 md:px-10">
-          <div className="lg:col-start-1 lg:col-span-2 w-3/4">
+        <div className="mt-6 grid grid-cols-1 gap-10 lg:gap-0 lg:grid-cols-5 w-full px-6 md:px-10">
+          <div className="mr-2">
             <div className="flex flex-col gap-4">
-              <h2 className="font-poppins-regular text-sm md:text-base mt-4">
-                Innovation distinguishes between a leader and a follower.
+              <h2 className="font-poppins-regular text-sm md:text-lg mt-4 font-bold">
+                Profil
               </h2>
-              <h2 className="font-poppins-bold text-sm md:text-base">
-                - Steve Jobs
+              <ul className="text-sm">
+                <li>- Tentang Kami</li>
+                <li>- Struktur Organisasi</li>
+                <li>- Pejabat Struktural</li>
+                <li>- Tugas Pokok dan Fungsi</li>
+                <li>- Doktrin Kejaksaan</li>
+                <li>- Visi dan Misi</li>
+                <li>- Perintah Harian Jaksa Agung</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mr-2">
+            <div className="flex flex-col gap-4">
+              <h2 className="font-poppins-regular text-sm md:text-lg mt-4 font-bold">
+                Profil Pimpinan
               </h2>
+              <ul className="text-sm">
+                <li>- Kepala Kejaksaan Negeri</li>
+                <li>- Kepala Sub Bagian Pembinaan</li>
+                <li>- Kepala Seksi Intelijen</li>
+                <li>- Kepala Saksi Tindak Pidana Umum</li>
+                <li>- Kepala Saksi Tindak Pidana Khusus</li>
+                <li>- Kepala Seksis Perdata & Tata Usaha Negara</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mr-2">
+            <div className="flex flex-col gap-4">
+              <h2 className="font-poppins-regular text-sm md:text-lg mt-4 font-bold">
+                Organisasi
+              </h2>
+              <ul className="text-sm">
+                <li>- Pembinaan</li>
+                <li>- Intelijen</li>
+                <li>- Tindak Pidana Umum</li>
+                <li>- Tindak Pidana Khusus</li>
+                <li>- Perdata & Tata Usaha Negara</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mr-2">
+            <div className="flex flex-col gap-4">
+              <h2 className="font-poppins-regular text-sm md:text-lg mt-4 font-bold">
+                Produk Hukum
+              </h2>
+              <ul className="text-sm">
+                <li>- Undang-Undang</li>
+                <li>- Putusan Mahkamah Konstitusi</li>
+                <li>- Peraturan Pemerintah</li>
+                <li>- Peraturan Presiden</li>
+                <li>- Keputusan Presiden</li>
+                <li>- Intruksi Presiden</li>
+                <li>- Peraturan Jaksa Agung</li>
+                <li>- Nota Kesepahaman</li>
+                <li>- Produk Hukum Lainnya</li>
+              </ul>
             </div>
           </div>
 
-          <div className="lg:col-end-4">
-            <h2 className="font-poppins-bold text-base uppercase">
-              Get In Touch
+          <div className="">
+            <h2 className="font-poppins-regular text-sm md:text-lg mt-4 font-bold">
+              Kontak
             </h2>
             <a
               href="mailto:febri@arkanatechnology.com"
               className="text-white"
               target="__blank"
             >
-              <div className="mt-2 flex font-poppins-medium text-base">
+              <div className="mt-2 flex font-poppins-medium text-sm">
                 <div className="text-xl pr-2 pt-1">
                   <FaLocationDot />
                 </div>
                 <div>
                   <p>
-                    Grand Harvest, Cluster Caspia CA-06, Balas Klumprik, Kec.
-                    Wiyung, Surabaya, Jawa Timur 60215
+                    Jalan Raya Sukomanunggal Jaya No.1, Sukomanunggal, Kota
+                    Surabaya, Jawa Timur 60188
                   </p>
                 </div>
               </div>
@@ -45,12 +98,12 @@ const Footer = () => {
               className="text-white"
               target="__blank"
             >
-              <div className="mt-2 flex font-poppins-medium text-base items-center">
+              <div className="mt-2 flex font-poppins-medium text-sm items-center">
                 <div className="text-xl pr-2">
                   <FiMail />
                 </div>
                 <div>
-                  <p>febri@arkanatechnology.com</p>
+                  <p>kejaksaan.surabaya@gmail.com</p>
                 </div>
               </div>
             </a>
@@ -59,18 +112,18 @@ const Footer = () => {
               className="text-white"
               target="__blank"
             >
-              <div className="mt-2 flex font-poppins-medium text-base">
+              <div className="mt-2 flex font-poppins-medium text-sm">
                 <div className="text-xl pr-2">
                   <FaWhatsapp />
                 </div>
-                <div>+62811333525</div>
+                <div>+031 7382297</div>
               </div>
             </a>
           </div>
         </div>
         <hr className="mt-20 mb-8 border-[#E0E0E0]" />
         <span className="block text-center font-poppins-medium text-sm px-6">
-          Copyright © 2023 - Arkana Technology | Powered by Arkana Technology
+          Copyright © 2024 - All Rights Reserved by Kejaksaan Negeri Surabaya
         </span>
       </div>
     </footer>
